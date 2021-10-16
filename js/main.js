58d018e29d67ec68fe3d28d8cf59b929fe8d14b5
Lukas Tomasek <vedstav31@gmail.com>
2021-10-15 20:27:17 -0400

    added more styling and logic

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const slider = document.getElementById('check')
+const feeIndicator = document.querySelector('.fee')
+const popularPlan = document.querySelector('.price-check')
+const logIn = document.getElementById('log-in')
+const modalWindow = document.querySelector('.modal')
+const promoBtn = document.getElementById('btn-bundles')
+
+slider.addEventListener('click', ()=>{ 
+  slider.checked == true ? feeIndicator.textContent = 'Annualy' : feeIndicator.textContent = 'Monthly'
+  slider.checked == true ? popularPlan.textContent = '$129.99/y' : popularPlan.textContent = '$12.99/mo'
+
+})
+
+let openModal = false
+
+function modalBehaviour(event,open){
+  const element = event.target
+  console.log(event.target)
+  if(open){
+    modalWindow.style.display = 'inline-block'
+    document.body.classList.add('stop-scrolling')
+  }else{
+    modalWindow.style.display = 'none'
+    document.body.classList.remove('stop-scrolling')
+  }
+}
+
+logIn.addEventListener('click', (event)=>{
+  openModal = !openModal
+  console.log(event)
+  modalBehaviour(event,openModal)
+})
+
+promoBtn.addEventListener('click',()=>{
+  let bundles  = document.querySelector('.deals')
+  bundles.scrollIntoView()
+  // window.scrollTo({top: 1500, 
+  //   behavior:"smooth"})
+})
